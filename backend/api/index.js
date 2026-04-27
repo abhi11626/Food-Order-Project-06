482724c4be75c3fda917d9eb5a715dd91e1baccd
Abhishek <gupta.abhi1987@gmail.com>
2026-04-27 23:08:21 +0530

    fix vercel api routing

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,18 @@
 // Import the Express app from the parent directory
-import app from '../app.js';
+// import app from '../app.js';
 
 // Export as default for Vercel serverless function
-export default app;
+// export default app;
 
+import app from "../app.js";
+
+export default function handler(req, res) {
+  // Fix URL
+  req.url = req.url.replace(/^\/api\/?/, "/");
+
+  // Important: call app as a function
+  return new Promise((resolve) => {
+    app(req, res);
+    res.on("finish", resolve);
+  });
+}
